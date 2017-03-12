bdc296e61e8b288b2b9097d706d5498a3e247760
libbyschuknight <libbysk@gmail.com>
2017-03-12 16:41:41 +1300

    Extract redundacny and prepre for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -18,26 +20,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update_attributes(article_params)
+    if set_article.update_attributes(article_params)
       flash[:notice] = "Article was successfully updated."
-      redirect_to article_path(@article)
+      redirect_to article_path(set_article)
     else
       render "new"
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    set_article.destroy
     flash[:notice] = "Article was successfully deleted."
     redirect_to articles_path
   end
@@ -45,5 +43,9 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
