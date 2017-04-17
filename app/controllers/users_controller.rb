16f78cef96a67ddbb18f664f656cc382ca921960
libbyschuknight <libbysk@gmail.com>
2017-04-17 15:42:17 +1200

    Fix up method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
 
   def index
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user && current_user.admin?
+    if current_user != @user && !current_user.admin?
       flash[:danger] = "You can only edit or delete your own profile."
       redirect_to root_path
     end
